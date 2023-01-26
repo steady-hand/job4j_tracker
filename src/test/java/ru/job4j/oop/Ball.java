9692afd29076180be56c1fcd50767e6123fc77fc
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-26 13:11:36 +0300

    1.6. Взаимодействие объектов [#166087 #66546]

---
+++ src/test/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean condition) {
+        if (condition == true) {
+            System.out.println("Хитрый колоб свалил");
+        } else {
+            System.out.println("Колоба сожрали, к сожалению");
+        }
+    }
+}
