4a77f8196d5bfc1c08ecdcd1450ff272d741a6d9
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-01-26 12:45:41 +0300

    1.5. Поля объекта [#166083 #66551]

---
+++ src/test/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+
+    public void show() {
+        System.out.println(this.food + " " + this.name );
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotletu el");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish el");
+        black.giveNick("black");
+        black.show();
+    }
+}
