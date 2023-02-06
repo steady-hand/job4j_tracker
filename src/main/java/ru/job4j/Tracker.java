8c7daffe1942adbaf45b5043ae0627f5a596f3dc
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-02-06 12:20:55 +0300

    5. Tracker - хранилище [#396 #66568]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -1,9 +1,12 @@
 package ru.job4j;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -21,5 +24,33 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] notNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                notNull[size] = items[i];
+                size++;
+            }
+        }
+        notNull = Arrays.copyOf(notNull, size);
+        return notNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] name = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null && item.getName() == key) {
+                name[size] = items[i];
+                size++;
+            }
+        }
+        name = Arrays.copyOf(name, size);
+        return name;
     }
 }
