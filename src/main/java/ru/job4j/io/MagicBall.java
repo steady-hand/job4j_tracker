071be1e37a15c1f12a9a3bea21229c9851194afd
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-02-08 12:33:00 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514 #66510]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String vopros = input.nextLine();
+        int answer = new Random().nextInt(3);
+        if (answer == 0) {
+            System.out.print("Да");
+        } else if (answer == 1) {
+            System.out.print("Нет");
+        } else {
+            System.out.print("Может быть");
+        }
+    }
+}
