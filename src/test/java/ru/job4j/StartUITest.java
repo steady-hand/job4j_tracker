376277f9740371b93d447032cda2804fbd3fe913
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-02-14 13:28:29 +0300

    9. Тестирование. Подготовка данных. [#182960 #66507]

--- src/test/java/ru/job4j/StartUITest.java
+++ src/test/java/ru/job4j/StartUITest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import ru.job4j.tracker.Input;
 import ru.job4j.tracker.StubInput;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.junit.Assert.assertNull;
 
 
 public class StartUITest {
@@ -16,5 +18,31 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "deleted item"
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertNull(deleted);
     }
 }
