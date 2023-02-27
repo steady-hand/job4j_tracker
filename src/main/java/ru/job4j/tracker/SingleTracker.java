c8a9c30f3233835b49c64997be7fa2ea543789b6
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-02-27 09:47:58 +0300

    1. Singleton [#94606 #66534]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+import ru.job4j.Item;
+import ru.job4j.Tracker;
+
+public class SingleTracker {
+    private Tracker tracker = new Tracker();
+    private static SingleTracker instance = null;
+    private SingleTracker() {
+    }
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+}
