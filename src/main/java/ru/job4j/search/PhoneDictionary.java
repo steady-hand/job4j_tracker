40563f225f4a9468121af5455b2bc60415f7f159
steady-hand <71493458+steady-hand@users.noreply.github.com>
2023-03-03 13:43:24 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #66485]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список пользователей, которые прошли проверку.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person value : persons) {
+            if (value.getPhone().contains(key) || value.getAddress().contains(key) || value.getName().contains(key)
+            || value.getSurname().contains(key)) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+}
