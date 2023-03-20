00038f911d2a55d4067c675a412a4d8944def1ce
steady_hand <v.gabrusenok10@gmail.com>
2023-03-20 09:55:00 +0300

    2. Преобразования List в Map. [#10093]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,7 +8,7 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean rsl = false;
+        boolean rsl;
         if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             rsl = true;
