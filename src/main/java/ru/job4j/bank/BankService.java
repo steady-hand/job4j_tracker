d5f46b6d1664cb4e84a939706450b0293c09b024
steady_hand <v.gabrusenok10@gmail.com>
2023-03-20 14:16:22 +0300

    3. Банковские переводы [#10038], доделать

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,74 @@
+package ru.job4j.bank;
+
+import ru.job4j.bank.Account;
+import ru.job4j.bank.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<Account>());
+        }
+    }
+    public boolean deleteUser(String passport) {
+        if (users.containsKey(findByPassport(passport))) {
+            users.remove(findByPassport(passport));
+            return true;
+        }
+        return false;
+    }
+
+    public void addAccount(String passport, Account account) {
+        if (users.containsKey(findByPassport(passport))) {
+            User user = findByPassport(passport);
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
+            }
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        if (users.get(findByPassport(passport)) != null) {
+            for (Account account : users.get(findByPassport(passport))) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = true;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+            if (!srcAccount.getRequisite().equals(srcRequisite)
+            || !destAccount.getRequisite().equals(destRequisite)
+            || srcAccount.getBalance() < amount) {
+                rsl = false;
+
+        }
+        return rsl;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
